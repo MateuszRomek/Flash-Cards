a9dbadb97b69808aadb728752f655de4f04ed4b9
Mateusz <mateusz.romek@outlook.com>
2020-03-21 16:54:17 +0100

    DOM elements object

---
+++ src/scripts/elements.js
@@ -0,0 +1,21 @@
+const elements = {
+    flipButton: document.querySelector('.card__flip'),
+    card: document.querySelector('.card'),
+    questionCard: document.querySelector('.front__title'),
+    answerCard: document.querySelector('.back__title'),
+    prevButton: document.querySelector('.prevBtn'),
+    nextButton: document.querySelector('.nextBtn'),
+    currentCardNumber: document.querySelector('.currentCard'),
+    allCardsNumber: document.querySelector('.allCards'),
+    modalOuter: document.querySelector('.modal__outer '),
+    form: document.querySelector('.form'),
+    questionInput: document.querySelector('.questionInput'),
+    answerInput: document.querySelector('.answerInput'),
+    formButton: document.querySelector('.submitButton'),
+    addNewCardButton: document.querySelector('.addButton'),
+    closeModalButton: document.querySelector('.closeModal'),
+    cardContainer: document.querySelector('.cardsContainer'),
+
+};
+
+export default elements;
